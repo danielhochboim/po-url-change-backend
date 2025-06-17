cb6d7fcbdb39b46f6d96198e33d1a6800e197073
danielhochboim <daniel.hochboim@gmail.com>
2025-06-17 16:56:12 +0300

    add middleware and change from put to patch

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from routers.channels_router import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",        # Next-dev
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # or ["*"] during local dev
+    allow_methods=["GET", "PATCH"], # list all verbs you use
+    allow_headers=["*"],            # let all headers through
+)
+
 
 app.include_router(router, prefix="/channels")
--- routers/channels_router.py
+++ routers/channels_router.py
@@ -5,26 +5,27 @@ import httpx
 import re
 
 router = APIRouter()
-
+channels={"channel 1" : "url1", "channel 2":"url2"} #for testing
 
 @router.get("/")
 async def get_channels():
     #all_channels = httpx.get("https://pi-api-url") # PI-API request to get all channel names and namespace
-    return {"data": "all_channels"}
+    return channels
 
 
 @router.get("/{channel_name}")
 async def get_channel_url(channel_name: str):
     #channel_url = httpx.get("https://pi-api-url") # PI-API request to get the a specific channel's URL
-    return {channel_name: 'channel_url'}
+    return channels[channel_name]
 
 
-@router.put("/{channel_name}")
+@router.patch("/{channel_name}")
 async def update_url(channel_name: str, url: schemas.URL):
 
     if re.search(r"^https?:\/\/.*", url.url):
         #httpx.put("https://pi-api-url", data={'url': url.url}) # PI-API request to put the url in the right place
-        return {channel_name: url.url}
+        channels[channel_name] = url.url
+        return channels[channel_name]
     
     else:
         return {"400": "Bad Request"}
